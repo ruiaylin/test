1a836b0ae790d7d418b6329468c1a891e4953f1e
alexstocks <alexstocks@foxmail.com>
2015-05-03 20:08:08 +0800

    change file header description

--- goext/src/container/xorlist/example_test.go
+++ goext/src/container/xorlist/example_test.go
@@ -1,6 +1,6 @@
-// Copyright 2013 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2015 AlexStocks(https://github.com/AlexStocks).
+// All rights reserved.  Use of this source code is
+// governed by a BSD-style license.
 
 package xorlist_test
 
