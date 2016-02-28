8aeacf3bfb26a3c6c86b5f02c7234c32fd4f26d4
alexstocks <alexstocks@foxmail.com>
2016-02-28 11:17:43 +0800

    del data backup

--- data_backup/scs-script/sys_path.py
+++
@@ -1,8 +0,0 @@
-"""
-#!/bin/python
-"""
-# written by AlexStocks on 2015/05/12 19:38pm
-import sys
-for i in sys.path:
-    print i
-
--- data_backup/split-log/readme.txt
+++
@@ -1,2 +0,0 @@
-crontab -l
-59 * * * * cd /home/disk1/scs && sh scs_log_split.sh scs_log_dir.conf
