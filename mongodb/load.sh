bde2cee3ba8f6675e505c3b93c5b9df9f9eb7637
AlexStocks <alexstocks@foxmail.com>
2016-12-19 16:22:58 +0800

    name -> PS_NAME

--- mongodb/load.sh
+++ mongodb/load.sh
@@ -9,7 +9,7 @@
 # FILE    : load.sh
 # ******************************************************
 
-name="mongod"
+PS_NAME="mongod"
 DB_PATH=${MONGODB_HOME}/data
 DB_HOST=localhost
 DB_PORT=10000
@@ -28,46 +28,46 @@ usage() {
 start() {
     # nohup mongod --dbpath=${DB_PATH} --logpath=${DB_PATH}/mongo.log --fork --logappend=true --rest --bind_ip=${DB_HOST} --port=${DB_PORT} --httpinterface  &
     nohup mongod --dbpath=${DB_PATH} --logpath=${DB_PATH}/mongo.log --fork --logappend --rest --bind_ip=${DB_HOST} --port=${DB_PORT} --httpinterface &
-	PID=`ps aux | grep -w $name | grep "${DB_PORT}" | grep -v grep | awk '{print $2}'`
+	PID=`ps aux | grep -w $PS_NAME | grep "${DB_PORT}" | grep -v grep | awk '{print $2}'`
     if [ "$PID" != "" ];
     then
         for p in $PID
         do
-            echo "start $name ( pid =" $p ")"
+            echo "start $PS_NAME ( pid =" $p ")"
         done
     fi
 	echo "web ui interface: http://${DB_HOST}:${WEB_UI_PORT}"
 }
 
 stop() {
-	PID=`ps aux | grep -w $name | grep "${DB_PORT}" | grep -v grep | awk '{print $2}'`
+	PID=`ps aux | grep -w $PS_NAME | grep "${DB_PORT}" | grep -v grep | awk '{print $2}'`
     if [ "$PID" != "" ];
     then
         for ps in $PID
         do
-            echo "kill -SIGINT $name ( pid =" $ps ")"
+            echo "kill -SIGINT $PS_NAME ( pid =" $ps ")"
 			kill -2 $ps
         done
     fi
 }
 
 term() {
-	PID=`ps aux | grep -w $name | grep "${DB_PORT}" | grep -v grep | awk '{print $2}'`
+	PID=`ps aux | grep -w $PS_NAME | grep "${DB_PORT}" | grep -v grep | awk '{print $2}'`
     if [ "$PID" != "" ];
     then
         for ps in $PID
         do
-            echo "kill -SIGKILL $name ( pid =" $ps ")"
+            echo "kill -SIGKILL $PS_NAME ( pid =" $ps ")"
 			kill -9 $ps
         done
     fi
 }
 
 list() {
-	PID=`ps aux | grep -w $name | grep "${DB_PORT}" | grep -v grep | awk '{printf("%s,%s,%s,%s\n", $1, $2, $9, $10)}'`
+	PID=`ps aux | grep -w $PS_NAME | grep "${DB_PORT}" | grep -v grep | awk '{printf("%s,%s,%s,%s\n", $1, $2, $9, $10)}'`
 	if [ "$PID" != "" ];
 	then
-		echo "list ${name} $role"
+		echo "list ${PS_NAME} $role"
 		echo "index: user, pid, start, duration"
 		idx=0
 		for ps in $PID
